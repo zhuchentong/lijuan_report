13cecc65289016ba036bcfa1c9942690c5dbb3d1
zhuchentong <2037630@163.com>
2026-08-14 12:08:04 +0800

    update

--- uno.config.ts
+++ uno.config.ts
@@ -2,7 +2,7 @@ import { defineConfig, presetWind3, presetAttributify } from 'unocss'
 
 export default defineConfig({
   presets: [
-    presetWind3(),
+    presetWind3({ important: '#app' }),
     presetAttributify(),
   ],
   theme: {
